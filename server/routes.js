8f78042afe1b66ee052a6cb2fb1bbf239384df07
Hristo <hristobak@gmail.com>
2017-12-02 22:21:00 +0100

    added find all sensor role

--- server/routes.js
+++ server/routes.js
@@ -43,6 +43,7 @@
 	app.post('/api/relations/user/role', relations.addUserRole);
 	app.put('/api/relations/user/:id/role',relations.updateRoleByUserId);
 
+	app.get('/api/relations/role/sensor/', relations.findAllRoleSensorRelations);
 	app.get('/api/relations/role/:id/sensor/', relations.findSensorsByRoleId);
 	app.post('/api/relations/role/:id/sensor', relations.setSensorsToRole);
 
