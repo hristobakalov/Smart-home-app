02b7d84a4323a9ed756c2633d24a73263cedaf0e
Hristo <hristobak@gmail.com>
2017-12-04 22:38:47 +0100

    adding password hash

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -23,7 +23,7 @@ var auth = {
     }
 	
 	helpers.cryptPassword(password, encryptedPassword);
-	
+	helpers.compare(password, '$2a$10$aHMmBjBFTcBCL6wO.pGrTONmXHvVu/ikBESc99q8HIBgg4tr1E22y', comparyPasswords)
     // Fire a query to your DB and check if the credentials are valid
     var dbUserObj = auth.validate(username, password, res);
 	
@@ -168,5 +168,12 @@ function encryptedPassword(err, hash){
 		console.log(err);
 	
 	console.log(hash);
+}
+
+function comparyPasswords(err, arePasswordsMatching){
+	if(err)
+		console.log(err);
+	
+	console.log(arePasswordsMatching);
 }
 module.exports = auth;
