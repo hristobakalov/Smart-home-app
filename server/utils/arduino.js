2adb98746bc7a7524a68553333b1b73c7b69c627
Hristo <hristobak@gmail.com>
2018-06-05 22:49:33 +0200

    stupid typo

--- server/utils/arduino.js
+++ server/utils/arduino.js
@@ -18,7 +18,7 @@ module.exports={
 		  // The whole response has been received. Print out the result.
 		  resp.on('end', () => {
 			console.log(JSON.parse(data));
-			cllback(data);
+			callback(data);
 		  });
 		 
 		}).on("error", (err) => {
